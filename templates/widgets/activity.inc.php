514bbe6174679c85efe38cafa93945995f6f3cff
Florian Steffens <flost@florians-mbp.fritz.box>
2014-10-04 19:36:55 +0200

    fix activitys

--- templates/widgets/activity.inc.php
+++ templates/widgets/activity.inc.php
@@ -1,39 +1,39 @@
-<div class="ocDashboard calendar items">
+<?php
 
-	<?php
+    function getNiceSmallText($string) {
+        $maxStringLen = 40;
+        $startStringLen = 5;
 
-	foreach ($additionalparams['activitys'] as $activity)
-	{	
-		print_unescaped(
-            "<div class='priority".$activity['priority']." activity-entry'>
+        if(strlen($string) >= $maxStringLen) {
+            $lastCharacter = -1 * ($maxStringLen-$startStringLen);
+            $return = substr($string,0,$startStringLen);
+            $return .= "...";
+            $return .= substr($string,$lastCharacter);
+        } else {
+            $return = $string;
+        }
+        return $return;
+    }
+
+?>
+
+<table>
+<tr><th>&nbsp;</th></tr>
+    <?php
+
+    /** @var $additionalparams array */
+    foreach ($additionalparams['activitys'] as $activity)
+    {
+        print_unescaped(
+            "<tr><td><div class='priority".$activity['priority']." activity-entry'>
                 <a href='".$activity["link"]."' title='".$activity['subject']."'>".\OC_Util::sanitizeHTML(getNiceSmallText($activity['subject'])).
                 "</a><br /><span> ".
                     \OCP\relative_modified_date(date("U",strtotime($activity['date']))).
                 "</span>
-            </div>
+            </div></td></tr>
         ");
-	}
-	?>		
-		
-</div>
-
-
-<?php
-
-function getNiceSmallText($string) {
-    $maxStringLen = 40;
-    $startStringLen = 5;
-
-    $return = "";
-    if(strlen($string) >= $maxStringLen) {
-        $lastCharacter = -1 * ($maxStringLen-$startStringLen);
-        $return = substr($string,0,$startStringLen);
-        $return .= "...";
-        $return .= substr($string,$lastCharacter);
-    } else {
-        $return = $string;
     }
-    return $return;
-}
 
-?>
+    ?>
+
+</table>
